e1e8173cdd2ba6b3b0218c961cc70349de1b911d
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2023-03-19 21:54:19 -0400

    Adding Files, nothing to show

---
+++ src/Prereq.svelte
@@ -0,0 +1,12 @@
+<script>
+    let prereq_path;
+</script>
+
+<h3>{prereq_path}</h3>
+
+
+<style>
+    h3 {
+        color: gold;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+import { onMount } from 'svelte';
+
+async function search_classes() {
+    let course_number = document.getElementById("drexel_class").value;
+    console.log(course_number);
+    fetch(`http://localhost:8000/prereqs_for/${course_number}`)
+        .then(response => {
+            let content = response.json()
+        })
+}
+</script>
+<input type="text" name="drexel_class" id="drexel_class" placeholder="Class">
+<input type="submit" value="Search" on:click={search_classes}>
