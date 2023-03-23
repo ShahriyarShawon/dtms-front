e4040fd6203f709dcb36ce25922c23d277ba057e
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2023-03-22 21:52:57 -0400

    Added some interfaces for pre and post reqs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,45 @@
+<svelte:head>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/500/fabric.min.js"></script>
+</svelte:head>
+
 <script>
 import { onMount } from 'svelte';
+import  course  from './course.svelte';
 
-async function search_classes() {
-    let course_number = document.getElementById("drexel_class").value;
-    console.log(course_number);
-    fetch(`http://localhost:8000/prereqs_for/${course_number}`)
-        .then(response => {
-            let content = response.json()
-        })
+let prereqs = [];
+let postreqs = [];
+
+async function searchPrereqsFor() {
+    const course_number = document.getElementById("course_name").value;
+
+    await fetch(`http://127.0.0.1:8000/prereqs_for/${course_number}`)
+        .then(response => response.json().then(data => {
+            prereqs = data;
+        }))
+}
+
+async function searchPostreqFor() {
+    const course_number = document.getElementById("postreq_course_name").value;
+
+    await fetch(`http://127.0.0.1:8000/postreq/${course_number}`)
+        .then(response => response.json().then(data => {
+            postreqs = data;
+        }))
 }
 </script>
-<input type="text" name="drexel_class" id="drexel_class" placeholder="Class">
-<input type="submit" value="Search" on:click={search_classes}>
+
+
+<h1>Hello</h1>
+<input type="text" id="course_name" placeholder="Enter Course to find its prereqs">
+<input type="submit" value="Search" on:click={searchPrereqsFor}> 
+
+{#each prereqs as obj, i}
+<li>{obj}</li>
+{/each}
+
+<input type="text" id="postreq_course_name" placeholder="Enter Course to find what classes this is a prereq for">
+<input type="submit" value="Search" on:click={searchPostreqFor}> 
+
+{#each postreqs as obj, i}
+<li>{obj}</li>
+{/each}
---
+++ src/routes/course.svelte
@@ -0,0 +1,15 @@
+<script>
+export let course_name;
+export let course_number;
+export let credits;
+export let prereqs;
+export let desc;
+</script>
+
+<div class="course">
+    <h1>{course_name}</h1>
+    <p>{course_number}</p>
+    <p>{credits}</p>
+    <p>{prereqs}</p>
+    <p>{desc}</p>
+</div>
